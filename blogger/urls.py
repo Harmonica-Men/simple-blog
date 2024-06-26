ed1721856ebdb9b08441e3b7e7f1b902fa0e1c7d
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-06-26 13:24:00 +0000

    #6 Django Blog - Django Forms With Bootstrap  - end

--- blogger/urls.py
+++ blogger/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #from . import views
-from .views import FrontpageView, BlogPostDetailView, AddPostView
+from .views import FrontpageView, BlogPostDetailView, AddPostView, UpdatePostView
 
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', FrontpageView.as_view(), name= 'frontpage'),
     path('article/<int:pk>', BlogPostDetailView.as_view(), name= 'article-detail'),
     path('add_post/', AddPostView.as_view(), name= 'add_post'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name= 'update-post'),
 ]
