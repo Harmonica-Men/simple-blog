0ae30b5f1dfae9998c86804aa5c9c59e76f0f930
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-06-26 12:50:01 +0000

    #6 Django Blog - Django Forms With Bootstrap  - begn

--- blogger/forms.py
+++ blogger/forms.py
@@ -11,5 +11,4 @@ class PostForm(forms.ModelForm):
             'title_tag' : forms.TextInput(attrs={'class': 'form-control'}),
             'author' : forms.Select(attrs={'class': 'form-control'}),
             'body' : forms.TextInput(attrs={'class': 'form-control'}),
-
         }
